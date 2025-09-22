307e929aa960301a69417d48750b4909a0be9a68
JEPC90 <judecrilly04@outlook.com>
2025-09-22 14:42:02 +0100

    Update proc_load_bronze.sql

--- scripts/bronze/proc_load_bronze.sql
+++ scripts/bronze/proc_load_bronze.sql
@@ -7,7 +7,7 @@ Stored Procedure: Load Bronze Layer (Source -> Bronze)
 
 Script Purpose:
 	This stored procedure loads data into the 'Bronze' schema from external CSV files.
-	It performs teh following actions:
+	It performs the following actions:
 	- Truncates the bronze tables before loading data.
 	- Uses the 'BULK INSERT'command to load data from csv files to bronze tables.
 
@@ -41,7 +41,7 @@ BEGIN
 
 		PRINT '>> Inserting Data Into: bronze.crm_cust_info';
 		BULK INSERT bronze.crm_cust_info
-		FROM 'C:\Users\judec\OneDrive\Desktop\My SQL 2025\sql-data-warehouse-project\datasets\source_crm\cust_info.csv'
+		FROM 'C:\Users\judec\OneDrive\Desktop\sql_learning\sql-data-warehouse-project\datasets\source_crm\cust_info.csv'
 		WITH (
 			FIRSTROW = 2,
 			FIELDTERMINATOR = ',',
@@ -57,7 +57,7 @@ BEGIN
 
 		PRINT '>> Inserting Data Into: bronze.crm_prd_info';
 		BULK INSERT bronze.crm_prd_info
-		FROM 'C:\Users\judec\OneDrive\Desktop\My SQL 2025\sql-data-warehouse-project\datasets\source_crm\prd_info.csv'
+		FROM 'C:\Users\judec\OneDrive\Desktop\sql_learning\sql-data-warehouse-project\datasets\source_crm\prd_info.csv'
 		WITH (
 			FIRSTROW = 2,
 			FIELDTERMINATOR = ',',
@@ -73,7 +73,7 @@ BEGIN
 
 		PRINT '>> Inserting Data Into: bronze.crm_sales_details';
 		BULK INSERT bronze.crm_sales_details
-		FROM 'C:\Users\judec\OneDrive\Desktop\My SQL 2025\sql-data-warehouse-project\datasets\source_crm\sales_details.csv'
+		FROM 'C:\Users\judec\OneDrive\Desktop\sql_learning\sql-data-warehouse-project\datasets\source_crm\sales_details.csv'
 		WITH (
 			FIRSTROW = 2,
 			FIELDTERMINATOR = ',',
@@ -93,7 +93,7 @@ BEGIN
 
 		PRINT '>> Inserting Data Into: bronze.erp_cust_az12';
 		BULK INSERT bronze.erp_cust_az12
-		FROM 'C:\Users\judec\OneDrive\Desktop\My SQL 2025\sql-data-warehouse-project\datasets\source_erp\CUST_AZ12.csv'
+		FROM 'C:\Users\judec\OneDrive\Desktop\sql_learning\sql-data-warehouse-project\datasets\source_erp\CUST_AZ12.csv'
 		WITH (
 			FIRSTROW = 2,
 			FIELDTERMINATOR = ',',
@@ -109,7 +109,7 @@ BEGIN
 
 		PRINT '>> Inserting Data Into: bronze.erp_loc_a101';
 		BULK INSERT bronze.erp_loc_a101
-		FROM 'C:\Users\judec\OneDrive\Desktop\My SQL 2025\sql-data-warehouse-project\datasets\source_erp\LOC_A101.csv'
+		FROM 'C:\Users\judec\OneDrive\Desktop\sql_learning\sql-data-warehouse-project\datasets\source_erp\LOC_A101.csv'
 		WITH (
 			FIRSTROW = 2,
 			FIELDTERMINATOR = ',',
@@ -125,7 +125,7 @@ BEGIN
 
 		PRINT '>> Inserting Data Into: bronze.erp_px_cat_g1v2';
 		BULK INSERT bronze.erp_px_cat_g1v2
-		FROM 'C:\Users\judec\OneDrive\Desktop\My SQL 2025\sql-data-warehouse-project\datasets\source_erp\PX_CAT_G1V2.csv'
+		FROM 'C:\Users\judec\OneDrive\Desktop\sql_learning\sql-data-warehouse-project\datasets\source_erp\PX_CAT_G1V2.csv'
 		WITH (
 			FIRSTROW = 2,
 			FIELDTERMINATOR = ',',
